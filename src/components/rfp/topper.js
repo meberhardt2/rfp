67ade8889db2e4c5ab24082efdfb7b8dea6bb934
meberhardt2 <matthew.eberhardt@gmail.com>
2017-10-11 11:19:25 -0500

    branch check

--- src/components/rfp/topper.js
+++ src/components/rfp/topper.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 /****************************************************************************************/
 export default class Topper extends React.Component {
-
 	/****************************************/
 	constructor(props) {
 		super(props);
